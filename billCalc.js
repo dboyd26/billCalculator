fa4b053fbd17ea27e60d14abe56a21f29632187f
dboyd26 <73416547+dboyd26@users.noreply.github.com>
2020-12-20 08:29:54 -0600

    button done
    finished button

--- billCalc.js
+++ billCalc.js
@@ -4,73 +4,68 @@
 
 
 
-function myBill()
-{
+function myBill() {
     let payment = document.getElementById("bill").value; //retrieve the cost of bill
-    console.log(payment);
     let tip; // placeholder for the tip
     let serviceX = document.getElementById("service");
     let service1 = serviceX.options[serviceX.selectedIndex].value;//shows the selected drop box option
-    console.log(service1);
-    
-    
-
     let numOfPeople = document.getElementById("people").value; //retrieve the amount of people
-    console.log(numOfPeople);
-      
-    let totalCost;
 
-   if (service1 == "outstanding") // want it to take the cost of the mean and depending on the rate of service, caluclate the percentage.
+
+
+
+    if (service1 == "outstanding") // want it to take the cost of the mean and depending on the rate of service, caluclate the percentage.
     {
         tip = (payment * 30 / 100);
-        
+
     }
 
-    else if (service1 == "good")
-    {
-        tip = (payment * 20 / 100) ;
+    else if (service1 == "good") {
+        tip = (payment * 20 / 100);
     }
 
-    else if (service1 == "ok")
-    {
+    else if (service1 == "ok") {
         tip = (payment * 15 / 100);
     }
 
-    else if (service1 == "bad")
-    {
+    else if (service1 == "bad") {
         tip = (payment * 10 / 100);
     }
 
-    else if (service1 == "terrible")
-    {
+    else if (service1 == "terrible") {
         tip = (payment * 5 / 100);
     }
-  
-    
 
-    
-    
-    totalCost = (tip / numOfPeople);
-    
-    
-    
-    
+    totalCost = (tip / numOfPeople).toFixed(2); //divide the percentage among the number of people
 
-    
-    
-    
-    document.getElementById("totalCost").innerHTML = "$" + totalCost;
-    
+    document.getElementById("peopleBtn").onclick = cost;
 
 
+    function cost() { // to display total seperately with a submit button
 
 
+        document.getElementById("total").innerHTML = "$" + totalCost; // prints out the answer
+
+    }
 
 
 
 
 
 }
+
+
+
+
+
+
+
+//document.getElementById("myBtn").addEventListener("click", function() {
+   // document.getElementById("demo").innerHTML = "Hello World";
+
+
+
+
 
 
 
